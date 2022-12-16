432ef3b90ae9b9e26a4af9670604f0c06aaeda9d
davidlopez <dfl11@alu.ua.es>
2022-12-16 18:10:44 +0100

    Hasta "El código que implementa crear nueva nota"

--- NotasCoreData/ViewController.swift
+++ NotasCoreData/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelFecha: UILabel!
+    @IBOutlet weak var labelMensaje: UILabel!
+    @IBOutlet weak var txtNota: UITextView!
+    
+    @IBAction func btnGuardar(_ sender: UIButton) {
+    }
+    @IBAction func btnCrear(_ sender: UIButton) {
+        labelFecha.text = ""
+        labelMensaje.text = ""
+        txtNota.text = ""
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
