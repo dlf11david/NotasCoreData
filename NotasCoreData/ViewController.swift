1df50fa47b096833bf4088b870ab574926437757
davidlopez <dfl11@alu.ua.es>
2022-12-16 18:17:20 +0100

    Parte I completa, pendiente parte II

--- NotasCoreData/ViewController.swift
+++ NotasCoreData/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtNota: UITextView!
     
     @IBAction func btnGuardar(_ sender: UIButton) {
+        guard let miDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let miContexto = miDelegate.persistentContainer.viewContext
+        let nuevaNota = Nota(context:miContexto)
+        nuevaNota.fecha = Date()
+        nuevaNota.texto = txtNota.text
+        do {
+           try miContexto.save()
+        } catch {
+           print("Error al guardar el contexto: \(error)")
+        }
+
+
     }
+    
     @IBAction func btnCrear(_ sender: UIButton) {
         labelFecha.text = ""
         labelMensaje.text = ""
