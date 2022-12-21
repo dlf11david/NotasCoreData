53fda9361c42012aac39230f416744d473ae18b9
davidlopez <dfl11@alu.ua.es>
2022-12-21 10:52:38 +0100

    Faltan los últimos 2 pasos

--- NotasCoreData/ListaNotasController.swift
+++ NotasCoreData/ListaNotasController.swift
@@ -28,20 +28,19 @@ class ListaNotasController: UITableViewController {
         let miContexto = miDelegate.persistentContainer.viewContext
         let notas = try! miContexto.fetch(request)
         for nota in notas {
-           print(nota.texto!)
+            listaNotas.append(nota)
+            print(nota.texto!)
         }
-
+        self.tableView.reloadData()
         
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return listaNotas.count
     }
 
 }
