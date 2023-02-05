b8b4d5a90d178b57be2ddf2fb60415cbc5ec39c3
davidlopez <dfl11@alu.ua.es>
2023-02-05 18:59:50 +0100

    Listar notas

--- NotasCoreData/ListaNotasCDController.swift
+++ NotasCoreData/ListaNotasCDController.swift
@@ -6,84 +6,52 @@
 //
 
 import UIKit
+import CoreData
 
 class ListaNotasCDController: UITableViewController {
+    
+    var frc : NSFetchedResultsController<Nota>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        let miDelegate = UIApplication.shared.delegate! as! AppDelegate
+        let miContexto = miDelegate.persistentContainer.viewContext
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let consulta = NSFetchRequest<Nota>(entityName: "Nota")
+        let sortDescriptors = [NSSortDescriptor(key:"fecha", ascending:false)]
+        consulta.sortDescriptors = sortDescriptors
+        self.frc = NSFetchedResultsController<Nota>(fetchRequest: consulta, managedObjectContext: miContexto, sectionNameKeyPath: nil, cacheName: "miCache")
+
+        //ejecutamos el fetch
+        try! self.frc.performFetch()
+        //Esto vendría también dentro del viewDidLoad, a continuación de lo anterior
+        if let resultados = frc.fetchedObjects {
+            print("Hay \(resultados.count) notas")
+            for nota in resultados {
+                print (nota.texto!)
+            }
+        }
+
     }
 
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return self.frc.sections!.count
     }
+
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.frc.sections![section].numberOfObjects
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "otraCelda", for: indexPath)
+        let nota = self.frc.object(at: indexPath)
+        cell.textLabel?.text = nota.texto!
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
