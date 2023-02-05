89343b365a81ff85483edaf855e47aca6f26c85a
davidlopez <dfl11@alu.ua.es>
2023-02-05 11:58:20 +0100

    Crear libretas

--- NotasCoreData/ViewController.swift
+++ NotasCoreData/ViewController.swift
@@ -38,6 +38,39 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func btnNuevaLibreta(_ sender: UIButton) {
+        nuevaLibreta()
+    }
+    
+    
+    func nuevaLibreta() {
+        let alert = UIAlertController(title: "Nueva libreta",
+                                      message: "Escribe el nombre para la nueva libreta",
+                                      preferredStyle: .alert)
+        let crear = UIAlertAction(title: "Crear", style: .default) {
+            action in
+            let nombre = alert.textFields![0].text!
+            guard let miDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            let miContexto = miDelegate.persistentContainer.viewContext
+            let nuevaLibreta = Libreta(context:miContexto)
+            nuevaLibreta.nombre = nombre
+            do {
+               try miContexto.save()
+            } catch {
+               print("Error al guardar el contexto: \(error)")
+            }
+        }
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel) {
+            action in
+        }
+        alert.addAction(crear)
+        alert.addAction(cancelar)
+        alert.addTextField() { $0.placeholder = "Nombre"}
+        self.present(alert, animated: true)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
